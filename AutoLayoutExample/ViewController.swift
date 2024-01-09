d27b6ad4ccf2b8305850a8146e8a63b446563b3c
Kanagasabapathy Rajkumar <40764138+sabapathyk7@users.noreply.github.com>
2024-01-10 00:14:29 +0530

    Initial Commit

---
+++ AutoLayoutExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutExample
+//
+//  Created by kanagasabapathy on 10/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
